3d603450e59ef71e57e56183bcd5461ed9a75dcb
Johann <johannth88@gmail.com>
2021-04-27 18:50:18 +0000

    score events

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ io.use((socket, next) => {
   next();
 });
 
+// {userId, userName, room}
 const users = [];
+const rooms = [];
 
 io.on('connection', (socket) => {
   const roomName = socket.handshake.headers.roomid;
@@ -35,8 +37,10 @@ io.on('connection', (socket) => {
   // for(let [id, socket] of io.of('/').sockets) {
   //   users.push({userID: id, username: socket.username, room: roomName}); // This sets all users room to the current room
   // }
-  
   users.push({userID: socket.id, username: socket.username, room: roomName});
+  if(!rooms.includes(roomName)) {
+    rooms.push(roomName);
+  }
 
   // TODO: BEFORE EMITING USERS, filter them out from the user array so that only current roomname users are sent out?
   // Todo: console log users on the frontend
@@ -60,11 +64,38 @@ io.on('connection', (socket) => {
 
   // Next question event
   socket.on('next question', (questionObj) => {
-    const {msg, status, room, question, currentIndex} = questionObj;
+    // const {msg, status, room, question, currentIndex} = questionObj;
+    const {room} = questionObj;
     console.log('Sending next question');
     // Send the next question to everyone in the room (except the sender (creator))
-    socket.to(room).emit('get next question', {msg, status, question, currentIndex});
+    socket.to(room).emit('get next question', questionObj);
   });
+
+  // Recieve what user has answered
+  socket.on('user answered', (userObj) => {
+    const creator = getRoomCreator(users, userObj.room);
+    if(creator) {
+      console.log('User answered sent');
+      io.to(creator.userID).emit('mark answered', {userId: userObj.userId});
+    }
+  });
+
+  // Process the answer and record history
+  socket.on('process answer', (answerObj) => {
+    // console.log('Forwarding: ', answerObj); 
+    // Getting the correct room creator to send the answer to
+    const creator = getRoomCreator(users, answerObj.room);
+    if(creator) {
+      io.to(creator.userID).emit('log answer', answerObj);
+    }
+  });
+
+  // Forwarding scores
+  socket.on('show scores', (scoreObj) => {
+    console.log('Recieved: ', scoreObj);
+    // Send the scores to everyone besides the creator
+    io.to(scoreObj.room).emit('get scores', scoreObj);
+  }); 
   
   // Notify users on disconnection
   socket.on('disconnect', () => {
@@ -85,4 +116,9 @@ server.listen(PORT, () => {
 const getUsersInRoom = (users, room) => {
   const filtered = users.filter((item) => item.room === room);
   return filtered;
+};
+
+const getRoomCreator = (users, room) => {
+  const found = users.find((user) => user.room === room && user.username === 'creator');
+  return found;
 };
